48d599f876a06ec876e8931f526fc62dd5f5433e
Ashwath Nanda <ashwath5897@gmail.com>
2023-04-25 12:19:05 +0530

    CodeDisplay, MessageDisplay components. Typescript init

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,20 @@
+import MessagesDisplay from "./components/MessagesDisplay";
+import CodeDisplay from "./components/CodeDisplay";
+
 const App = () => {
   return (
     <div className="App">
+      <MessagesDisplay/>
+      <input/>
+      <CodeDisplay/>
+      <div>
+        <button id="get-query">
+          Generate!
+        </button>
+        <button id="clear-chat">
+          Clear
+        </button>
+      </div>
     </div>
   );
 }
