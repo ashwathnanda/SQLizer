550fa634b765829434fe4b0e42981e1e4a9436ed
Ashwath Nanda <ashwath5897@gmail.com>
2023-04-28 20:14:17 +0530

    Basic Express app for completion

--- index.ts
+++ index.ts
@@ -1,8 +1,38 @@
-import express, { Application, Request, Response } from "express";
+import express, {Application, Request, Response} from "express";
 import cors from "cors";
-import { Configuration, OpenAIApi } from "openai";
+import {Configuration, OpenAIApi} from "openai";
 
 const PORT: number = 8080;
 const API_KEY: string = ''
 
-// TODO: Expand to a separate service.
+const app: Application = express()
+app.use(cors)
+app.use(express.json())
+
+
+const configuration = new Configuration({
+    apiKey: API_KEY
+})
+
+const openai = new OpenAIApi(configuration)
+
+app.post("/completions", async (req: Request, res: Response) => {
+    try {
+        const completion = await openai.createChatCompletion({
+            model: "gpt-4",
+            messages: [
+                {
+                    role: "user",
+                    content: "Create a SQL Request to " + req.body.message
+                }]
+        })
+        res.send(completion.data.choices[0].message)
+
+    } catch (error) {
+        res.status(500).send("Server Error")
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running in ${PORT}`)
+})
